83c50831587c00122ac76fd582d682714381c6cd
PinkSheep <oli.fav@bluewin.ch>
2014-03-25 15:40:35 +0100

    Chapter 6

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140311141111) do
+ActiveRecord::Schema.define(version: 20140325141111) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
