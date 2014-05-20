ae52259501ce91f8e5e4dfb6fb038c28f9270d88
PinkSheep <oli.fav@bluewin.ch>
2014-05-20 15:14:25 +0200

    commit all missing features

--- app/helpers/store_helper.rb
+++ app/helpers/store_helper.rb
@@ -1,2 +1,9 @@
 module StoreHelper
+#  def number_to_chf(price)
+#    number_to_currency(price, unit: "CHF", format: "%n %u")
+#  end
+  def number_to_currency(number, options={})
+    options[:locale] ||= I18n.locale
+    super(number, options)
+  end
 end
